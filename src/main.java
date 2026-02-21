89748e61d7638963e82f68d9f809205c40133006
Raffay irfan <raffayirfan2008@gmail.com>
2026-02-21 20:09:49 +0500

    changed main from source file to class

--- src/main.java
+++ src/main.java
@@ -1,13 +1,15 @@
 import javax.swing.*;
 
-void main() {
-    System.setProperty("sun.java2d.opengl", "true");
-    JFrame frame =new JFrame("fractal level 8");
-    frame.setDefaultCloseOperation(3);
-    //frame.setSize(600,600);
-    frame.setResizable(false);
-    frame.add(new sirpinski(frame));
-    frame.pack();
-    frame.setVisible(true);
-    frame.requestFocusInWindow();
+public class main {
+    public static void main(String[] args) {
+        System.setProperty("sun.java2d.opengl", "true");
+        JFrame frame =new JFrame("fractal level 8");
+        frame.setDefaultCloseOperation(3);
+        //frame.setSize(600,600);
+        frame.setResizable(false);
+        frame.add(new sirpinski(frame));
+        frame.pack();
+        frame.setVisible(true);
+        frame.requestFocusInWindow();
+    }
 }
